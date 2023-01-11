42c0e70027d92295713b07c4ab1978be0a9719ce
Humza Shahid <humzasaur@gmail.com>
2023-01-11 19:54:58 +0000

    scaffold

---
+++ src/RopeNode.fs
@@ -0,0 +1,23 @@
+﻿namespace AppendRope
+
+open Types
+
+module RopeNode =
+    let inline concat string node =
+        node.String <- node.String + string
+
+    let inline addLeft leftDelta node =
+        node.LeftIdx <- node.LeftIdx + leftDelta
+
+    let inline addRight rightDelta node =
+        node.RightIdx <- node.RightIdx + rightDelta
+
+    let inline setIdx left right node =
+        node.LeftIdx <- left
+        node.RightIdx <- right
+
+    type RopeNode with
+        member inline this.Concat string = concat string this
+        member inline this.AddLeft delta = addLeft delta this
+        member inline this.AddRight delta = addRight delta this
+        member inline this.SetIdx left right = setIdx left right this
---
+++ src/Types.fs
@@ -0,0 +1,23 @@
+﻿namespace AppendRope
+
+module Types =
+    type RopeNode = {
+        mutable String: string;
+        mutable LeftIdx: int;
+        mutable RightIdx: int;
+    }
+
+    type RopeTree =
+        | E
+        | T of int * RopeTree * RopeNode * RopeTree
+
+    type AppendRope = {
+        mutable Tree: RopeTree;
+        mutable TextLength: int;
+        mutable LineCount: int;
+    }
+
+    (* When we are appending, if the last node contains a string less than this,
+     * then we concatenate; else, we create a new node. *)
+    [<Literal>]
+    let TargetNodeLength = 1024
