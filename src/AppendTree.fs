a066703b5c709608f68055859677c00eb0e33cc2
Humza Shahid <humzasaur@gmail.com>
2023-01-12 10:14:06 +0000

    update append function to handle concatenation if less than TargetNodeLength

--- src/AppendTree.fs
+++ src/AppendTree.fs
@@ -12,10 +12,15 @@ module AppendTree =
     /// Returns an empty AppendTree.
     let empty = E
 
+    /// Append a string to an AppendTree.
     let append string node = 
         let rec insMax node cont =
             match node with
             | E -> T(1, E, RopeNode.create string, E)
+            | T(h, l, v, E) when v.String.Length < TargetNodeLength ->
+                v.Concat string
+                T(h, l, v, E)
+                |> skew |> split |> cont
             | T(h, l, v, r) -> 
                 insMax r (fun r' ->
                     v.AddRight string.Length
