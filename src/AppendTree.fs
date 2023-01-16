e21809ca6bb9fc3e350057eeec9746e272e03956
Humza Shahid <74657165+hummy123@users.noreply.github.com>
2023-01-16 19:22:12 +0000

    add substring function on append rope

--- src/AppendTree.fs
+++ src/AppendTree.fs
@@ -24,3 +24,44 @@ module AppendTree =
                     |> skew |> split |> cont
                 )
         insMax node topLevelCont
+
+    /// Returns a substring from the rope at the given start index and length.
+    let substring (start: int) (length: int) rope =
+        let finish = start + length
+        let acc = ResizeArray<char>() (* Using mutable array for performance. *)
+        
+        (* To do: maybe convert to CPS. *)
+        let rec sub curIndex node =
+            match node with
+            | E -> ()
+            | T(h, l, v, r) ->
+                if start < curIndex
+                then sub (curIndex - stringLength l - sizeRight l) l
+                let nextIndex = curIndex + v.String.Length
+
+                if start <= curIndex && finish >= nextIndex then 
+                    (* Node is fully in range. *)
+                    for i in v.String do
+                        acc.Add i
+                elif start >= curIndex && finish <= nextIndex then
+                    (* Range is within node. *)
+                    let strStart = start - curIndex
+                    let length = finish - start
+                    for i in v.String.Substring(strStart, length) do
+                        acc.Add i
+                elif finish < nextIndex && finish >= curIndex then
+                    (* Start of node is within range. *)
+                    let length = finish - curIndex
+                    for i in v.String.Substring(0, length) do
+                        acc.Add i
+                elif start > curIndex && start <= nextIndex then
+                    (* End of node is within range. *)
+                    let strStart = start - curIndex
+                    for i in v.String[strStart..] do
+                        acc.Add i
+
+                if finish > nextIndex
+                then sub (nextIndex + sizeLeft r) r
+
+        sub (sizeLeft rope) rope
+        new string(acc.ToArray())
