0c2bf0eff071bfb24312760046f8d1275069f375
Humza Shahid <74657165+hummy123@users.noreply.github.com>
2023-01-20 09:43:53 +0000

    add failing test case

--- test/Rope_GetLine_Tests.fs
+++ test/Rope_GetLine_Tests.fs
@@ -42,3 +42,39 @@ let ``Rope.GetLine returns correct segments when we delete line breaks in comple
     Assert.Equal("Lorem ipsumdolor sit amet,\n", rope.GetLine 0)
     Assert.Equal("consecteturadipiscing elit. \n", rope.GetLine 1)
     Assert.Equal("Aenean ornare, lacus vitae \n", rope.GetLine 2)
+
+
+[<Fact>]
+let ``Rope.GetLine returns correct segments when we delete multiple line breaks at first half`` () =
+    let str = "Lorem ipsum\ndolor sit amet,\nconsectetur\nadipiscing elit. \nAenean ornare, \nlacus vitae \ntempor pretium,\nleo nulla\nsollicitudin elit,\nin ultrices mi dui et\nipsum. Cras condimentum\npurus in metus \nsodales tincidunt. Praesent"
+    let rope = PieceRope.create str
+
+    // delete "\ndolor sit amet,\n"
+    let rope = rope.Delete(11, 17)
+    let str = str.Remove(11, 17)
+
+    // check text is same after deletion (side-check to ensure consistency)
+    Assert.Equal(str, rope.Text())
+
+    Assert.Equal("Lorem ipsumconsectetur\n", rope.GetLine 0)
+    Assert.Equal("adipiscing elit. \n", rope.GetLine 1)
+
+    // delete "\nlacus vitae \ntempor pretium,\n"
+    let rope = rope.Delete(57, 29)
+    let str = str.Remove(57, 29)
+    Assert.Equal(str, rope.Text())
+
+    // previos assertions to check they still work
+    Assert.Equal("Lorem ipsumconsectetur\n", rope.GetLine 0)
+    Assert.Equal("adipiscing elit. \n", rope.GetLine 1)
+
+    // current assertion
+    Assert.Equal("Aenean ornare, \n", rope.GetLine 2)
+    Assert.Equal("leo nulla\n", rope.GetLine 3)
+
+    // assertions for lines after to check they still work as expected
+    Assert.Equal("sollicitudin elit,\n", rope.GetLine 4)
+    Assert.Equal("in ultrices mi dui et\n", rope.GetLine 5)
+    Assert.Equal("ipsum. Cras condimentum\n", rope.GetLine 6)
+    Assert.Equal("purus in metus \n", rope.GetLine 7)
+    Assert.Equal("sodales tincidunt. Praesent", rope.GetLine 8)
