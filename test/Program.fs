7023949893b0db47541140db5aab6c75ef25052b
Humza Shahid <74657165+hummy123@users.noreply.github.com>
2023-01-22 20:01:06 +0000

    set up failing case for bug fixing

--- test/Program.fs
+++ test/Program.fs
@@ -1,14 +1,34 @@
 open PieceRope
 open PieceRope.PieceRope
 open System
+open FsCheck
+
+
+// Initial data
+let lorem = "Lorem ipsum\ndolor sit amet,\nconsectetur\nadipiscing elit. \nAenean ornare, \nlacus vitae \ntempor pretium,\nleo nulla\nsollicitudin elit,\nin ultrices mi dui et\nipsum. Cras condimentum\npurus in metus \nsodales tincidunt. Praesent"
+let initRope = PieceRope.create lorem
+let initString = lorem
+
+// minimal failing case: insert
+// 27, \n
+// 207, \n
+// line 12 is different
 
 module Program =
     [<EntryPoint>]
     let main _ =  
-        let str = "abcdefghij"
-        let rope = PieceRope.create str
-    
-        let rope = rope.Insert(4, "\n")
-    
-        let line = rope.GetLine 0
+        let mutable testString = initString
+        let mutable testRope = initRope
+
+        testRope <- testRope.Insert(27, "\n")
+        testString <- testString.Insert(27, "\n")
+
+        // no longer same after this insert
+        testRope <- testRope.Insert(207, "\n")
+        testString <- testString.Insert(207, "\n")
+
+        let spliString = testString.Split("\n")
+        let failingLine = testRope.GetLine 12
+        printfn "is loop same? %A" (spliString[12] + "\n" = failingLine)
+
         0
