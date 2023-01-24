eae9e2dda45dd3adf60c7457543bcd8dafcae464
Humza Shahid <74657165+hummy123@users.noreply.github.com>
2023-01-24 16:42:55 +0000

    Revert "remove crdt"
    This reverts commit 4b3cc50d7f66e12a7d1eacd0f0292de67824f87b.

---
+++ CrdtBenchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Running;
+using CrdtBenchmarks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieceRopeCrdtBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PieceRopeBenchmarks>();
+        }
+
+    }
+}
