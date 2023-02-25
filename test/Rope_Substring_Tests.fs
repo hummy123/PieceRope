682e63cbd219769d4437bfd8162d3eb281bde00f
Humza Shahid <humza@pop-os.localdomain>
2023-02-25 11:41:21 +0000

    add substring tests, all passing

--- test/Rope_Substring_Tests.fs
+++ test/Rope_Substring_Tests.fs
@@ -2,8 +2,7 @@
 
 open System
 open Xunit
-open PieceRope
-open PieceRope.PieceRope
+open HumzApps.TextDocument
 
 [<Literal>]
 let text =
@@ -12,132 +11,146 @@ let text =
 [<Literal>]
 let insText = "TEST!"
 
-let initialTable = PieceRope.create text
+let initialTable = TextDocument.create text
 
 [<Fact>]
 let ``Can get a substring from the start of a table's OriginalBuffer`` () =
-    let substring = initialTable.Substring(0, 2)
+    let substring = TextDocument.substring 0 2 initialTable
     Assert.Equal("Du", substring)
 
 [<Fact>]
 let ``Can get a substring from the whole of a table's AddBuffer`` () =
-    let table = initialTable.Insert(5, insText)
-    let substring = table.Substring(5, insText.Length)
+    let substring =
+      TextDocument.insert 5 insText initialTable
+      |> TextDocument.substring 5 insText.Length
     Assert.Equal("TEST!", substring)
 
 [<Fact>]
 let ``Can get a substring from around a table's AddBuffer`` () =
-    let table = initialTable.Insert(5, insText)
-    let substring = table.Substring(4, insText.Length + 2)
+    let substring =
+      TextDocument.insert 5 insText initialTable
+      |> TextDocument.substring 4 (insText.Length + 2)
     Assert.Equal("nTEST!g", substring)
 
 [<Fact>]
 let ``Can get a substring from the end of a table's OriginalBuffer`` () = 
-    let substring = initialTable.Substring(text.Length - 5, 5)
+    let substring = TextDocument.substring (text.Length - 5) 5 initialTable 
     Assert.Equal(text.Substring(text.Length - 5, 5), substring)
 
 [<Fact>]
 let ``Can get a substring from the end of a table's AddBuffer`` () =
-    let table = PieceRope.create ""
-    let table = table.Insert(0, text)
-    let substring = table.Substring(text.Length - 5, 5)
+    let substring = 
+      TextDocument.create ""
+      |> TextDocument.insert 0 text
+      |> TextDocument.substring (text.Length - 5) 5
     Assert.Equal(text.Substring(text.Length - 5, 5), substring)
 
 [<Fact>]
 let ``Can get a substring from the middle of a table's OriginalBuffer`` () = 
-    let substring = initialTable.Substring(1, 1)
+    let substring = TextDocument.substring 1 1 initialTable
     let expectedStr = "u"
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring from the middle of a table's AddBuffer`` () =
-    let table = initialTable.Insert(1, "abc")
-    let substring = table.Substring(2,1)
+    let substring = 
+      TextDocument.insert 1 "abc" initialTable
+      |> TextDocument.substring 2 1
     let expectedStr = "b"
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring from start when zipper is at end`` () =
-    let table = initialTable.Insert(text.Length, insText)
-    let substring = table.Substring(0, 10)
+    let substring = 
+      TextDocument.insert text.Length insText initialTable
+      |> TextDocument.substring 0 10
     let expectedStr = text.Substring(0, 10)
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring from the middle when zipper is at end`` () =
-    let table = initialTable.Insert(text.Length, insText)
-    let substring = table.Substring(10, 20)
+    let substring =
+      TextDocument.insert text.Length insText initialTable
+      |> TextDocument.substring 10 20
     let expectedStr = text.Substring(10, 20)
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring from end when zipper is at end`` () =
-    let table = initialTable.Insert(text.Length, insText)
-    let substring = table.Substring(text.Length, insText.Length)
+    let substring =
+      TextDocument.insert text.Length insText initialTable
+      |> TextDocument.substring text.Length insText.Length
     let expectedStr = insText
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring from start when zipper is in middle`` () =
-    let table = initialTable.Insert(text.Length/2, insText)
-    let substring = table.Substring(0,5)
+    let substring =
+      TextDocument.insert (text.Length / 2) insText initialTable
+      |> TextDocument.substring 0 5
     let expectedStr = text.Substring(0, 5)
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get substring of middle when zipper is in middle`` () =
-    let table = initialTable.Insert(text.Length/2, insText)
-    let substring = table.Substring(text.Length/2, insText.Length)
+    let substring =
+      TextDocument.insert (text.Length / 2) insText initialTable
+      |> TextDocument.substring (text.Length / 2) insText.Length
     let expectedStr = insText
     Assert.Equal(expectedStr, substring)
 
 [<Fact>]
 let ``Can get a substring over a split piece`` () =
-    let table = PieceRope.create "123456789"
-    let table = table.Insert(0, "a")
-    let table = table.Insert(2, "b")
-    let substr = table.Substring(0, 3)
-    Assert.Equal("a1b", substr)
+    let substring = 
+      TextDocument.create "123456789"
+      |> TextDocument.insert 0 "a"
+      |> TextDocument.insert 2 "b"
+      |> TextDocument.substring 0 3
+    Assert.Equal("a1b", substring)
 
 [<Fact>]
 let ``TextEdit1`` () =
     // Enter string correctly.
-    let table = PieceRope.create "124567893"
-    let table = table.Insert(8, "98")
-    let table = table.Insert(9, "ab")
+    let table = 
+      TextDocument.create "124567893"
+      |> TextDocument.insert 8 "98"
+      |> TextDocument.insert 9 "ab"
+
     let expWholestr = "124567899ab83"
-    let wholestr = table.Text()
+    let wholestr = TextDocument.text table
     // Check we have entered string correctly.
     Assert.Equal(expWholestr, wholestr)
 
     // Get substring.
     let expsubstr9 = "124567899"
-    let substr9 = table.Substring(0, 9)
+    let substr9 = TextDocument.substring 0 9 table
     Assert.Equal(expsubstr9, substr9)
 
 [<Fact>]
 let ``TextEdit2`` () =
     // Enter string correctly.
-    let table = PieceRope.create "123456789"
-    let table = table.Insert(8, "a")
-    let table = table.Insert(9, "b")
-    let table = table.Insert(7, "c")
-    let table = table.Insert(8, "d")
-    let table = table.Insert(6, "e")
-    let table = table.Insert(7, "f")
+    let table = 
+      TextDocument.create "123456789"
+      |> TextDocument.insert 8 "a"
+      |> TextDocument.insert 9 "b"
+      |> TextDocument.insert 7 "c"
+      |> TextDocument.insert 8 "d"
+      |> TextDocument.insert 6 "e"
+      |> TextDocument.insert 7 "f"
+
     let expWholestr = "123456ef7cd8ab9"
-    let wholestr = table.Text()
+    let wholestr = TextDocument.text table
     // Check we have entered string correctly.
     Assert.Equal(expWholestr, wholestr)
 
     let expsubstr = "123456e"
-    let substr = table.Substring(0, 7)
+    let substr = TextDocument.substring 0 7 table
     Assert.Equal(expsubstr, substr)
 
     let expsubstr = "123456ef"
-    let substr = table.Substring(0, 8)
+    let substr = TextDocument.substring 0 8 table
     Assert.Equal(expsubstr, substr)
 
     let expsubstr = "123456ef7"
-    let substr = table.Substring(0, 9)
+    let substr = TextDocument.substring 0 9 table
     Assert.Equal(expsubstr, substr)
