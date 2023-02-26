14ea0eb6d2f45d5d8b9bccef261800c1f63fac06
Humza Shahid <humza@pop-os.localdomain>
2023-02-26 08:46:11 +0000

    add undo/redo stack functionality

--- src/TextDocument.fs
+++ src/TextDocument.fs
@@ -12,10 +12,13 @@ open System.Globalization
 type TextDocument = {
   Buffer: PieceBuffer;
   Pieces: PieceTree;
+  UndoStack: PieceTree list;
+  RedoStack: PieceTree list;
+  ShouldAddToHistory: bool;
 }
   with
-  member inline this.Length                   = PieceTree.size this.Pieces
   member inline this.Lines                    = PieceTree.lines this.Pieces
+  member inline this.Length                   = PieceTree.size this.Pieces
 
   member inline this.Insert(index, string)    = TextDocument.insert index string this
   member inline this.Delete(start, length)    = TextDocument.delete start length this
@@ -26,10 +29,22 @@ type TextDocument = {
   member inline this.GetLine line             = TextDocument.getLine line this
   member inline this.Text()                   = TextDocument.text this
 
+  member inline this.AddToHistory()          = TextDocument.addToHistory this
+  member inline this.CanUndo                  = TextDocument.canUndo this
+  member inline this.CanRedo                  = TextDocument.canRedo this
+  member inline this.Undo()                   = TextDocument.undo this
+  member inline this.Redo()                   = TextDocument.redo this
+
 /// The TextDocument module provides functions for inserting into, deleting from and querying ranges of text.
 [<RequireQualifiedAccess>]
 module TextDocument =
-  let empty = { Buffer = PieceBuffer.empty; Pieces = PieceTree.empty }
+  let empty = { 
+    Buffer = PieceBuffer.empty; 
+    Pieces = PieceTree.empty;
+    UndoStack = [];
+    RedoStack = [];
+    ShouldAddToHistory = false;
+  }
 
   (* Preprocess string to generate an array of line breaks and a lookup table for grapheme clusters. *)
   let inline private preprocessString (string: string) pcStart =
@@ -57,7 +72,14 @@ module TextDocument =
       let (lineBreaks, charBreaks) = preprocessString string pcStart
       let buffer = PieceBuffer.append string charBreaks document.Buffer
       let pieces = PieceTree.insert index pcStart charBreaks.Length lineBreaks document.Pieces
-      { Buffer = buffer; Pieces = pieces; }
+
+      let undo =
+        if document.ShouldAddToHistory then
+          document.Pieces::document.UndoStack
+        else
+          document.UndoStack
+      let redo = []
+      { Buffer = buffer; Pieces = pieces; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false }
 
   let prepend (string: string) document =
     if String.IsNullOrEmpty string then
@@ -67,7 +89,14 @@ module TextDocument =
       let (lineBreaks, charBreaks) = preprocessString string pcStart
       let buffer = PieceBuffer.append string charBreaks document.Buffer
       let pieces = PieceTree.prepend pcStart charBreaks.Length lineBreaks document.Pieces
-      { Buffer = buffer; Pieces = pieces; }
+
+      let undo =
+        if document.ShouldAddToHistory then
+          document.Pieces::document.UndoStack
+        else
+          document.UndoStack
+      let redo = []
+      { Buffer = buffer; Pieces = pieces; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false }
     
   let append (string: string) document =
     if String.IsNullOrEmpty string then
@@ -77,14 +106,28 @@ module TextDocument =
       let (lineBreaks, charBreaks) = preprocessString string pcStart
       let buffer = PieceBuffer.append string charBreaks document.Buffer
       let pieces = PieceTree.append pcStart charBreaks.Length lineBreaks document.Pieces
-      { Buffer = buffer; Pieces = pieces; }
+
+      let undo =
+        if document.ShouldAddToHistory then
+          document.Pieces::document.UndoStack
+        else
+          document.UndoStack
+      let redo = []
+      { Buffer = buffer; Pieces = pieces; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false }
 
   let delete (start: int) (length: int) document =
     if length <= 0 then
       document
     else
       let pieces = PieceTree.delete start length document.Pieces
-      { document with Pieces = pieces; }
+
+      let undo =
+        if document.ShouldAddToHistory then
+          document.Pieces::document.UndoStack
+        else
+          document.UndoStack
+      let redo = []
+      { document with Pieces = pieces; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false }
 
   let create string = append string empty
 
@@ -96,4 +139,35 @@ module TextDocument =
 
   let text document =
     PieceTree.getText document.Pieces document.Buffer
+
+  let canUndo document =
+    match document.UndoStack with
+    | [] -> false
+    | _ -> true
+
+  let canRedo document =
+    match document.RedoStack with
+    | [] -> false
+    | _ -> true
+
+  let undo document = 
+    if canUndo document then
+      let redo = document.Pieces::document.RedoStack
+      let current = document.UndoStack.Head
+      let undo = document.UndoStack.Tail
+      { document with Pieces = current; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false; }
+    else
+      document
+
+  let redo document =
+    if canRedo document then
+      let current = document.RedoStack.Head
+      let redo = document.RedoStack.Tail
+      let undo = document.Pieces::document.UndoStack
+      { document with Pieces = current; UndoStack = undo; RedoStack = redo; ShouldAddToHistory = false; }
+    else
+      document
+
+  let addToHistory document =
+    { document with ShouldAddToHistory = true; }
 
